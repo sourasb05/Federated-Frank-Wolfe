832a7f354f8aad2685a48a9172fb66d390ae2212
sourasb05 <sourasb05@gmail.com>
2023-03-10 17:48:52 +0100

    model updated

---
+++ model.py
@@ -0,0 +1,125 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# CNN model for MNIST
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, 2, 1)
+        self.conv2 = nn.Conv2d(16, 32, 2, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(18432, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.ReLU()(x)
+        x = nn.MaxPool2d(2, 1)(x)
+        x = self.dropout1(x)
+        x = self.conv2(x)
+        x = nn.ReLU()(x)
+        x = nn.MaxPool2d(2, 1)(x)
+        x = self.dropout2(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = nn.ReLU()(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+    
+
+# CNN model for CIFAR10 
+
+class CifarNet(nn.Module):
+    def __init__(self):
+        super(CifarNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+        self.dropout = nn.dropout(.2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        # x = self.dropout(x)
+        return F.log_softmax(x, dim=1)
+
+
+# CNN model for FMNIST
+
+class cnn_fmnist(nn.Module):
+    
+    def __init__(self, args):
+        super(cnn_fmnist, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
+        self.fc1 = nn.Linear(in_features=12 * 4 * 4, out_features=120)
+        self.fc2 = nn.Linear(in_features=120, out_features=60)
+        self.fc3 = nn.Linear(in_features=60, out_features=40)
+        self.out = nn.Linear(in_features=40, out_features=10)
+
+    def forward(self, x):
+        # input layer
+        x = x
+        # first hidden layer
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        # second hidden layer
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        # third hidden layer
+        x = x.reshape(-1, 12 * 4 * 4)
+        x = self.fc1(x)
+        x = F.relu(x)
+        # fourth hidden layer
+        x = self.fc2(x)
+        x = F.relu(x)
+        # fifth hidden layer
+        x = self.fc3(x)
+        x = F.relu(x)
+        # output layer
+        x = self.out(x)
+        return x
+
+
+# DNN for Synthetic datasets
+
+class DNN(nn.Module):
+    def __init__(self, input_dim = 784, mid_dim = 100, output_dim = 10):
+        super(DNN, self).__init__()
+        # define network layers
+        self.fc1 = nn.Linear(input_dim, mid_dim)
+        self.fc2 = nn.Linear(mid_dim, output_dim)
+        
+    def forward(self, x):
+        # define forward pass
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+    
+# Model for LASSO
+
+# Model for MCLR
+
+# Model for QAP
+
+# Model for matrix Complition 
+

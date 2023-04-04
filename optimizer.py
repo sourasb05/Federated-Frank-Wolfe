7dc4124a347c30b274697b432fdb347aac106560
sourasb05 <sourasb05@gmail.com>
2023-04-04 11:40:54 +0200

    sgd added

---
+++ optimizer.py
@@ -0,0 +1,24 @@
+from torch.optim import Optimizer
+
+
+class MySGD(Optimizer):
+    def __init__(self, params, lr):
+        defaults = dict(lr=lr)
+        super(MySGD, self).__init__(params, defaults)
+
+    def step(self, closure=None, beta = 0):
+        loss = None
+        if closure is not None:
+            loss = closure
+
+        for group in self.param_groups:
+            # print(group)
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                d_p = p.grad.data
+                if(beta != 0):
+                    p.data.add_(-beta, d_p)
+                else:     
+                    p.data.add_(-group['lr'], d_p)
+        return loss

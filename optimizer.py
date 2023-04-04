93ac9ca87cf1b51eb71efaf34181f37c3b18f2d0
sourasb05 <sourasb05@gmail.com>
2023-04-04 12:55:48 +0200

    Sgd updated (removed beta)

--- optimizer.py
+++ optimizer.py
@@ -17,6 +17,27 @@ class MySGD(Optimizer):
                 if p.grad is None:
                     continue
                 d_p = p.grad.data
+                p.data = p.data - (group['lr'] * d_p)
+        
+        return loss
+    
+
+class MyGD(Optimizer):
+    def __init__(self, params, lr):
+        defaults = dict(lr=lr)
+        super(MySGD, self).__init__(params, defaults)
+
+    def step(self, closure=None, beta = 0):
+        loss = None
+        if closure is not None:
+            loss = closure
+
+        for group in self.param_groups:
+            # print(group)
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                d_p = p.grad.data
                 if(beta != 0):
                     p.data.add_(-beta, d_p)
                 else:     

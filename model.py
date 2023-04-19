0977da5786ac83b9c90fc2703a1ee373cfa45497
sourasb05 <sourasb05@gmail.com>
2023-04-19 19:50:43 +0200

    cifar100 model added

--- model.py
+++ model.py
@@ -134,7 +134,7 @@ class cnn_Emnist(nn.Module):
 # CNN for CELEBA dataset
 
 
-class CNN_Celeba(nn.Module):
+class cnn_Celeba(nn.Module):
     def __init__(self):
         super(CNN_Celeba, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
@@ -174,7 +174,57 @@ class CNN_Celeba(nn.Module):
         x = self.fc2(x)
         return x
 
-    
+
+# CNN for Cifar-100 
+
+class cnn_Cifar100(nn.Module):
+    def __init__(self):
+        super(cnn_Cifar100, self).__init__()
+        
+        # Define the layers of the CNN
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(num_features=32)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(num_features=64)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(num_features=128)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.fc1 = nn.Linear(in_features=4*4*128, out_features=256)
+        self.relu4 = nn.ReLU()
+        
+        self.fc2 = nn.Linear(in_features=256, out_features=100)
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+        
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+        
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu3(x)
+        x = self.pool3(x)
+        
+        x = x.view(-1, 4*4*128)
+        x = self.fc1(x)
+        x = self.relu4(x)
+        x = self.fc2(x)
+        
+        return x
+
 # Model for LASSO
 
 # Model for MCLR

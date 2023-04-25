717a10a52c1d963f0ce7cc6a9119a42b281f4b53
sourasb05 <sourasb05@gmail.com>
2023-04-25 15:07:16 +0200

    matrix complition added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from model import *
 from server import Fed_Avg_Server
 from client import Fed_Avg_Client
 from utils import select_users, read_data, read_user_data #, RMSE_nuclear
+from src.MatrixComp import My_matrix_comp
 import argparse
 import torch.nn as nn 
 
@@ -9,78 +10,97 @@ def main(dataset, model, fl_algorithm, optimizer, fl_aggregator, step_size, glob
     exp_no=0
 
     device = torch.device("cuda:{}".format(gpu) if torch.cuda.is_available() and gpu != -1 else "cpu")
-
+    
     while exp_no < times:
-        if model == "CNN":
-            if dataset == "MNIST":
-                model = cnn_Mnist().to(device), model
-                loss = nn.NLLLoss()
-                
-            elif dataset == "FMNIST":
-                model = cnn_Fmnist().to(device), model
-                loss = nn.NLLLoss()
+    
+        if model == "CNN" or model == "MCLR" or model == "DNN" or model == "LASSO":
+            
+            if model == "CNN":
+                if dataset == "MNIST":
+                    model = cnn_Mnist().to(device), model
+                    loss = nn.NLLLoss()
+                        
+                elif dataset == "FMNIST":
+                    model = cnn_Fmnist().to(device), model
+                    loss = nn.NLLLoss()
 
-            elif dataset == "CIFAR10":
-                model = cnn_Cifar10().to(device), model
-                loss = nn.NLLLoss()
-            
-            elif dataset == "EMNIST":
-                model = cnn_Emnist().to(device), model
-                loss = nn.NLLLoss()
-            
-            elif dataset == "CELEBA":
-                model = cnn_Celeba().to(device), model
-                loss = nn.NLLLoss()
+                elif dataset == "CIFAR10":
+                    model = cnn_Cifar10().to(device), model
+                    loss = nn.NLLLoss()
+                    
+                elif dataset == "EMNIST":
+                    model = cnn_Emnist().to(device), model
+                    loss = nn.NLLLoss()
+                    
+                elif dataset == "CELEBA":
+                    model = cnn_Celeba().to(device), model
+                    loss = nn.NLLLoss()
 
-            elif dataset == "CIFAR100":
-                model = cnn_Cifar100().to(device), model
-                loss = nn.NLLLoss()
-            
-            elif dataset == "MOVIELENS_1m" and dataset == "MOVIELENS_100k":
-                # model = MatrixFactorization(num_users, num_movies, embedding_size)
-                print("model is in progress....")
-            
-        else:
-            print(" do nothing")
+                elif dataset == "CIFAR100":
+                    model = cnn_Cifar100().to(device), model
+                    loss = nn.NLLLoss()
+                        
+            else:
+                print(" do nothing")
+            problem_category = 1
+
+        elif model == "MATRIX_COMP":
+            print("line 48")
+            if  dataset == "MOVIELENS_100k" or dataset == "MOVIELENS_1m":
+                data = read_data(dataset)
+                # print(data)
+                r = My_matrix_comp.matrix_comp(data)
+                input("press")
+            problem_category = 2
         
-        if fl_algorithm == "FedAvg":
-            users = []   # the list of the object of the users
-            data = read_data(dataset) 
-            # print(data)
-            input("interrupt from line 50 in main")
+    
+        elif model == "QAP":
+            print("code building in progress....")
 
-            # print("len_data",len(data[1]))
-            # input("press")
-            """
-            data[0] : client id
-            data[1] : train data
-            data[2] : test data
-            
-            """
-              # read_dataset will return 
-            total_users = len(data[0])  
-            server = Fed_Avg_Server(fl_aggregator, model)
-            for i in range(0,total_users):
-                train, test = read_user_data(i,data,dataset)
-                # print("len(train)", len(train))
-                # print(i)
-                data_ratio = len(data[1])/len(train)
-                # print("data_ratio",data_ratio) ## The ratio is not fixed yet
-                user = Fed_Avg_Client(model, optimizer, loss, train, test, local_iters, step_size, batch_size, data_ratio, device)   # Creating the instance of the users. 
-                users.append(user)
-            # print(users)
-            
-        for i in range(0,glob_iters):
-            print("----- Global iteration [",i,"]-----")
-            server.send_parameters(users)   # server send parameters to every users
-            server.evaluate(users)  # evaluate global model
-            selected_users = select_users(users)
-            #print("len of selected users",len(selected_users))
-            for user in selected_users:
-                user.local_train()
 
-            server.global_update(users, selected_users)
-            
+
+        else:
+            print("do nothing")
+
+        if problem_category == 1:
+            if fl_algorithm == "FedAvg":
+                users = []   # the list of the object of the users
+                data = read_data(dataset) 
+                # print(data)
+                input("interrupt from line 50 in main")
+
+                # print("len_data",len(data[1]))
+                # input("press")
+                """
+                data[0] : client id
+                data[1] : train data
+                data[2] : test data
+                
+                """
+                # read_dataset will return 
+                total_users = len(data[0])  
+                server = Fed_Avg_Server(fl_aggregator, model)
+                for i in range(0,total_users):
+                    train, test = read_user_data(i,data,dataset)
+                    # print("len(train)", len(train))
+                    # print(i)
+                    data_ratio = len(data[1])/len(train)
+                    # print("data_ratio",data_ratio) ## The ratio is not fixed yet
+                    user = Fed_Avg_Client(model, optimizer, loss, train, test, local_iters, step_size, batch_size, data_ratio, device)   # Creating the instance of the users. 
+                    users.append(user)
+                # print(users)
+                
+            for i in range(0,glob_iters):
+                print("----- Global iteration [",i,"]-----")
+                server.send_parameters(users)   # server send parameters to every users
+                server.evaluate(users)  # evaluate global model
+                selected_users = select_users(users)
+                #print("len of selected users",len(selected_users))
+                for user in selected_users:
+                    user.local_train()
+
+                server.global_update(users, selected_users)
+                
 
 
 
